dece72f39e2a4b29b31a1187918b29f374c7d9b6
dannybintangprayogo <dannybaserah055@gmail.com>
2024-05-07 01:16:22 -1200

    Tugas 2

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <h1>{{ greeting }}</h1>
+    <p>{{ message }}</p>
+    <input type="text" v-model="inputText">
+    <div :class="{ active: isActive }" :style="{ color: textColor }">Elemen dengan class dan style bindings</div>
+    <button @click="changeMessage">Ubah Pesan</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      greeting: 'Halo!',
+      message: 'Ini adalah pesan default.',
+      isActive: true,
+      textColor: 'black',
+      inputText: ''
+    }
+  },
+  methods: {
+    changeMessage() {
+      this.message = this.inputText; // Mengubah pesan sesuai dengan input teks
+      this.isActive = !this.isActive; // Toggle class active
+      this.textColor = this.isActive ? 'black' : 'red'; // Ganti warna teks
+    }
+  }
+}
+</script>
+
+<style scoped>
+.active {
+  font-weight: bold;
+}
+</style>
